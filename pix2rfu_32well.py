9663f68c49f5dda1ca72bb0efdd61f32c47578bb
Young Paik <ympaik@gmail.com>
2019-04-08 17:45:35 +0900

    add all features; need to fix bugs

--- pix2rfu_32well.py
+++ pix2rfu_32well.py
@@ -1,5 +1,5 @@
 import argparse
-# from srt_rfu.srt_rfu32 import SrtRfu32
+from srt_rfu.srt_rfu32 import SrtRfu32
 
 
 def main(args):
@@ -7,18 +7,22 @@ def main(args):
     print('path', args.exp_path)
     print('except col', args.except_col)
     print('except row', args.except_row)
-    print('TC', args.tc, int(args.tc)-1)
     if args.is_onefile:
         print('is onefile')
-    # _rfu = SrtRfu32(args.path)
-    # _rfu.get_datasheet()
+    _rfu = SrtRfu32(args.exp_path)
+    if args.tc:
+        print('TC', args.tc, int(args.tc)-1)
+        tc = int(args.tc) - 1
+        _rfu.get_datasheet(tc)
+    else:
+        _rfu.get_datasheet()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Convert image results from SRT to RFU formatted'
         'for DSP analysis')
-    parser.add_argument('--version', action='version', version='0.0.1')
+    parser.add_argument('-v', '--version', action='version', version='0.0.1')
     parser.add_argument(
         'exp_path', help='Path of the experiment directory, '
         'which contains main and sub folders for result images. '
@@ -31,9 +35,11 @@ if __name__ == '__main__':
         title='onefile', dest='is_onefile',
         description='get image processing result from a file')
     parser_onef = subparsers.add_parser('i')
-    parser_onef.add_argument('cam', choices=['main', 'sub'])
     parser_onef.add_argument('temp', choices=['low', 'high'])
     parser_onef.add_argument('dye', choices=['f', 'h', 'c', 'q6', 'q7'])
     parser_onef.add_argument('cycle')
+    well_li = [x+z+str(y+1) for x in list(
+        'ABCDabcd') for y in range(8) for z in ['', '0']]
+    parser_onef.add_argument('well', choices=well_li)
     args = parser.parse_args()
     main(args)
--- srt_rfu/srt_rfu32.py
+++ srt_rfu/srt_rfu32.py
@@ -1,9 +1,16 @@
 import pathlib
-import skimage
+import xlsxwriter
+import datetime
+from skimage.filters import threshold_otsu
+from skimage.measure import regionprops, label
+from skimage.morphology import closing, opening, disk
+from skimage.segmentation import clear_border
+from skimage.color import label2rgb
 import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib import patches
 
 
 class SrtRfu32:
@@ -20,13 +27,12 @@ class SrtRfu32:
             'q7': 'Quasar 705',
             'h': 'HEX'
         }
-        self.row_name = {'main': list('ABCD'), 'sub': list('EFGH')}
+        self.row_name = list('ABCD')
         self.cam_keys = ['main', 'sub']
 
     def get_region_dic(self, im_labeled, im_gray):
         region_dic = {}
-        for region in skimage.measure.regionprops(
-                im_labeled, intensity_image=im_gray):
+        for region in regionprops(im_labeled, intensity_image=im_gray):
             region_dic[region.area] = region
         return region_dic
 
@@ -47,16 +53,17 @@ class SrtRfu32:
                 pts_given = np.array([x, y])
                 distance = np.linalg.norm(pts_given-pts_center)
                 if distance < radius:
-                    return well
+                    return well, radius
         return None
 
-    def calculate_rfu(self, region_dic, cam):
+    def calculate_rfu(self, region_dic, cam, ax=None):
         "calculate RFU by image"
         region_sum_dict = {}
         for key in self.grid[cam].keys():
             region_sum_dict[key] = 0
         areas_li = list(region_dic.keys()).sort(reverse=True)
         center_at_cycle = {}
+        circle_li = []
         for area in areas_li:
             region_obj = region_dic[area]
             y, x = region_obj.centroid
@@ -67,12 +74,25 @@ class SrtRfu32:
             if grid not in center_at_cycle.keys():
                 center = [x, y]
                 center_at_cycle[grid] = center
+                if ax:
+                    well, rad = self.get_well_name(x, y, center, cam)
+                    circle_li.append(patches.Circle(
+                        center, radius=rad, color='r', fill=False,
+                        linewidth=1))
             else:
                 center = center_at_cycle[grid]
-            well = self.get_well_name(x, y, center, cam)
+            well, rad = self.get_well_name(x, y, center, cam)
 
             if well is not None:
                 region_sum_dict[well] += region_obj.intensity_image.sum()
+                if ax:
+                    ax.plot(x, y, color='white', marker='*')
+                    ax.text(x, y, well, color='gray')
+            elif ax:
+                ax.plot(x, y, color='b', marker='x')
+        if ax:
+            for circle in circle_li:
+                ax.add_artist(circle)
         return region_sum_dict
 
     def label_image(self, im_path):
@@ -80,14 +100,13 @@ class SrtRfu32:
         im_cropped = im[self.x_range, self.y_range]
 
         im_gray = im_cropped.sum(axis=2)
-        thresh = skimage.filters.threshold_otsu(im_gray)
+        thresh = threshold_otsu(im_gray)
         threshed_im = im_gray > thresh
 
-        bw = skimage.morphology.closing(
-            threshed_im, skimage.morphology.disk(3))
-        bw2 = skimage.morphology.opening(bw, skimage.morphology.disk(3))
-        cleared = skimage.segmentation.clear_border(bw2)
-        return skimage.measure.label(cleared), im_gray
+        bw = closing(threshed_im, disk(3))
+        bw2 = opening(bw, disk(3))
+        cleared = clear_border(bw2)
+        return label(cleared), im_gray
 
     def set_grid(self, tc=44):
         "get grid by camera from the last cycle"
@@ -97,34 +116,37 @@ class SrtRfu32:
         sub_grid_im_path = self.exp_path/'{}/{}_0_f.jpg'.format(
             self.cam_keys[1], tc)
         for idx, im_path in enumerate([main_grid_im_path, sub_grid_im_path]):
-            im_labeled, im_gray = self.label_image(im_path)
-            region_dic = self.get_region_dic(im_labeled, im_gray)
-            areas_li = list(region_dic.keys()).sort(reverse=True)
-            bbox_key_li = ['minr', 'minc', 'maxr', 'maxc']
-            bbox_dic = {}
-            for key in bbox_key_li:
-                bbox_dic[key] = []
-            for key2 in areas_li[:16]:
-                region = region_dic[key2]
-                bbox_li = region.bbox
-                for i, k in enumerate(bbox_key_li):
-                    bbox_dic[k] = bbox_li[i]
-            well_box = []
-            for key3 in bbox_key_li[:2]:
-                well_box.append(bbox_dic[key3].sort()[0]-50)
-            for key4 in bbox_key_li[2:]:
-                well_box.append(bbox_dic[key4].sort()[-1]+50)
-            x_li = np.linspace(well_box[1], well_box[3], 5, endpoint=True)
-            y_li = np.linspace(well_box[0], well_box[2], 5, endpoint=True)
-            pts_li = [(x, y) for x in x_li for y in y_li]
-            for ind in range(16):
-                top_left_pt = pts_li[ind]
-                bottom_right_pt = pts_li[ind+6]
-                i, j = divmod(ind, 4)
-                key5 = self.row_name[self.cam_keys[idx]][i] + str(j+1)
-                self.grid[self.cam_keys[idx]][key5] = [
-                    top_left_pt[1], top_left_pt[0],
-                    bottom_right_pt[1], bottom_right_pt[0]]
+            self.set_grid_single(im_path, idx)
+
+    def set_grid_single(self, im_path, idx):
+        im_labeled, im_gray = self.label_image(im_path)
+        region_dic = self.get_region_dic(im_labeled, im_gray)
+        areas_li = list(region_dic.keys()).sort(reverse=True)
+        bbox_key_li = ['minr', 'minc', 'maxr', 'maxc']
+        bbox_dic = {}
+        for key in bbox_key_li:
+            bbox_dic[key] = []
+        for key2 in areas_li[:16]:
+            region = region_dic[key2]
+            bbox_li = region.bbox
+            for i, k in enumerate(bbox_key_li):
+                bbox_dic[k] = bbox_li[i]
+        well_box = []
+        for key3 in bbox_key_li[:2]:
+            well_box.append(bbox_dic[key3].sort()[0]-50)
+        for key4 in bbox_key_li[2:]:
+            well_box.append(bbox_dic[key4].sort()[-1]+50)
+        x_li = np.linspace(well_box[1], well_box[3], 5, endpoint=True)
+        y_li = np.linspace(well_box[0], well_box[2], 5, endpoint=True)
+        pts_li = [(x, y) for x in x_li for y in y_li]
+        for ind in range(16):
+            top_left_pt = pts_li[ind]
+            bottom_right_pt = pts_li[ind+6]
+            i, j = divmod(ind, 4)
+            key5 = self.row_name[i] + str(j+1+idx*4)
+            self.grid[self.cam_keys[idx]][key5] = [
+                top_left_pt[1], top_left_pt[0],
+                bottom_right_pt[1], bottom_right_pt[0]]
 
     def make_rfu_table(self, tc=44):
         "concatenate rfu by camera, dye, temp, cycle"
@@ -132,27 +154,73 @@ class SrtRfu32:
         for ind, temp in enumerate(self.temp_li):
             self.rfu_dict[temp] = {}
             for dye_abb, dye in self.ch_dict.items():
-                self.rfu_dict[temp][dye] = {}
-                for cam in self.cam_keys:
+                _dic2 = {}
+                for cycle in range(tc):
                     _dic = {}
-                    for cycle in range(tc):
+                    for cam in self.cam_keys:
                         im_path = self.exp_path/'{}/{}_{}_{}.jpg'.format(
                             cam, cycle, ind, dye_abb)
                         im_labeled, im_gray = self.label_image(im_path)
                         region_dic = self.get_region_dic(im_labeled, im_gray)
                         _rfu = self.calculate_rfu(region_dic, cam)
-                        _dic[cycle+1] = _rfu
-                self.rfu_dict[temp][dye] = pd.DataFrame(_dic).T
+                        _dic.update(_rfu)
+                    _dic2[cycle+1] = _dic
+                self.rfu_dict[temp][dye] = pd.DataFrame(_dic2).T
 
-    def get_datasheet(self):
-        "save rfu table as xlsx for for DSP analysis"
+    def make_end_point_results(self, path):
+        suffix = ' -  End Point Results.xlsx'
+        well_li = [
+            x+'0'+str(y+1) for x in list('ABCD') for y in range(8)][::-1]
+        with xlsxwriter.Workbook(path/(self.exp_path.name+suffix)) as writer:
+            ws = writer.add_worksheet()
+            ws.write(0, 1, 'Well')
+            for i, well in enumerate(well_li):
+                ws.write(i+1, 1, well)
+
+    def get_datasheet(self, tc=44):
+        "save rfu table as xlsx for DSP analysis"
         suffix = ' -  Quantitation Amplification Results.xlsx'
+        qs_li = ['QuantStep60', 'QuantStep72']
+        self.set_grid(tc=tc)
+        self.make_rfu_table(tc=tc)
+        res_dir = self.exp_path/'DSP_datasheet'
+        if res_dir.exists():
+            res_dir = self.exp_path/(
+                'DSP_datasheet' + datetime.datetime.now().strftime(
+                    '_%Y%m%d_%H%M%S'))
+        res_dir.mkdir()
+        for ind, temp in enumerate(self.temp_li):
+            qs_path = res_dir/qs_li[ind]
+            with pd.ExcelWriter(qs_path/(self.exp_path.name+suffix)) as writer:
+                for dye, df in self.rfu_dict[temp].items():
+                    df.to_excel(writer, sheet_name=dye)
+            self.make_end_point_results(qs_path)
 
-    def get_onef_result(self, cam, tmp, dye, cycle):
+    def get_onef_result(self, tmp, dye, cycle, well, tc=44):
         "save image processing result in image file (by cycle)"
+        self.set_grid(tc=tc)
+        if 'low' in tmp.lower():
+            ind = 0
+        else:
+            ind = 1
         ind = self.temp_li.index(tmp)
+        col = int(well[-1])
+        if 0 <= col < 5:
+            cam = self.cam_keys[0]
+        else:
+            cam = self.cam_keys[1]
         im_path = self.exp_path/'{}/{}_{}_{}.jpg'.format(
             cam, cycle, ind, dye)
         im_labeled, im_gray = self.label_image(im_path)
-        image_label_overlay = skimage.color.label2rgb(
+        image_label_overlay = label2rgb(
             im_labeled, bg_label=0, colors=self.colors_li)
+        region_dic = self.get_region_dic(im_labeled, im_gray)
+
+        fig, ax = plt.subplots(1, 3, figsize=(24, 12))
+        ax[0, 0].imshow(im_gray)
+        ax[0, 1].imshow(im_labeled)
+        ax[0, 2].imshow(image_label_overlay)
+        self.calculate_rfu(region_dic, cam, ax[0, 2])
+        plt.tight_layout()
+        plt.savefig(self.exp_path/'Processed_result_{}_{}_{}_{}.jpg'.format(
+            cam, cycle, ind, dye))
