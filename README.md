8322798732e51c24c02969e53200f13d52120944
Young Paik <ympaik@gmail.com>
2019-07-16 09:57:46 +0900

    add srt_rfu16 dummy version

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # PIX2RFU
 
 ## How to use
+
 ### 1. Make Datasheet in CFX96 format from the real-time PCR images.
+
 To create a datasheet from entire images
-```
+
+```bash
 $ activate rfu
 $ cd rfu
 # for 32 well plate
@@ -11,11 +14,14 @@ $ python pix2rfu_32well.py "F:/path to experiment directory"
 # for 96 well plate
 $ python pix2rfu_96well.py "F:/path to experiment directory"
 ```
+
 It will create `DSP_datasheet` folder in the experiment path, and save datasheets according to the CFX96 format.
 
 #### For 96 well
+
 `pix2rfu_96well.py` is for images from 96 plate well, and names of folder should be the following:
-```
+
+```text
 exp_path
    +-- front_left
    +-- front_right
@@ -26,7 +32,8 @@ exp_path
 ```
 
 if there are missing dye,
-```
+
+```bash
 $ python pix2rfu_32well.py "F:/path to experiment directory" -d <missing dyes>
 # for example, if FAM and HEX channels are missing
 $ python pix2rfu_32well.py "F:/path to experiment directory" -d f h
@@ -43,16 +50,19 @@ Quasar 670 | `q6`
 Quasar 705 | `q7`
 
 **(UPCOMING)** if there are missing columns or rows
-```
+
+```bash
 $ python pix2rfu_32well.py "F:/path to experiment directory" -c <numbers of missing cols> -r <numbers of missing rows>
 ```
 
 ### 2. Get image processing result from an image
 
 To look into the image processing result
-```
+
+```bash
 $ python pix2rfu_32well.py "F:/path to experiment directory" i <temp> <dye> <cycle> <well>
 ```
+
 where `temp` is Low or High, `dye` is one of `['f', 'h', 'c', 'q6', 'q7']`, well is well name such as A1 and b3, and cycle is a number between 1 and TC (total cycle).
 
 Intended Value | Valid Input
@@ -70,31 +80,35 @@ Cycle number | Any number from 1 to total cycle. e.g. `2`
 This will produce the following image.
 ![Result Image](doc/Result_a0519f2-sub_44_0_h.jpg)
 It is composed of 4 images, and one data table.
-  * Original: the original image from camera with cropping area as a red rectangle
-  * Gray: the 2D density plot from cropped image with sum of RGB
-  * Labeled: the image of labeled objects after image processing the gray image
-  * Processed Result: the result image with objects with well location
-  * Data table: table of RFU values, which are sum of RGB, corresponding to their well location
+
+* Original: the original image from camera with cropping area as a red rectangle
+* Gray: the 2D density plot from cropped image with sum of RGB
+* Labeled: the image of labeled objects after image processing the gray image
+* Processed Result: the result image with objects with well location
+* Data table: table of RFU values, which are sum of RGB, corresponding to their well location
 
 **It will be more helpful to identify causes of issues if you are attaching these images as posting a new issue**
 
 ### 3. Get image processing result from an image of single camera test
+
 If you wish to take a look at an image processing result with single image for the test purpose, try the following feature:
-```
+
+```bash
 $ python pix2rfu_32well.py "F:/path to a 16 well image" s
 ```
+
 This will produce the similar image processing result as the above except the part that it creates the well grid (green dot areas) from the current images instead of the reference image.
 
 ### 4. Get RFU values from test run
 If you have run a test with a few cycles of multiple conditions, this program also can generate excel files of their RFU table for each condition.
 
-```
+```bash
 $ python exp2rfu.py "F:/path to a 16 well image"
 ```
 
 In order to run this, first, you have to organize image files as following:
 
-```
+```text
 exp_path
    +-- <condition1 with any name>
       +-- <Dye>_<Cycle>_<any suffix>.jpg
@@ -119,13 +133,16 @@ Keep in mind that dye and cycle should be separated by underscore, `_`, in order
 This function will produce excel files in `DSP_datasheet_<datetime>` folder with corresponding to names of their experiment conditions.
 
 ## Requirements
+
 ### Environment
-- Python 3.5.3
-- Conda
-- git
+
+* Python 3.5.3
+* Conda
+* git
 
 ### Installation with virtual environment (Windows)
-```
+
+```bash
 $ git clone https://github.com/ympaik87/rfu.git
 $ conda create -n rfu python=3.5.3
 $ activate rfu
@@ -133,14 +150,25 @@ $ cd rfu
 $ pip install -r requirements.txt
 ```
 
-### To update the code
+### Installation (Linux)
+
+```bash
+# library dependency for scikit-image
+$ sudo apt-get install libatlas-base-dev
+$ sudo apt-get install python3-matplotlib python3-numpy python3-pil python3-scipy python3-tk
+$ sudo apt-get install build-essential cython3
+$ pip install -r requirements_rp3.txt
 ```
+
+### To update the code
+
+```bash
 $ git pull
 ```
 
 If you have local changes, and it causes to abort `git pull`, one way to get around this is the following:
 
-```
+```bash
 # removing the local changes
 $ git stash
 # update
---
+++ srt_rfu/srt_rfu16-dummy.py
@@ -0,0 +1,145 @@
+import subprocess
+import pathlib
+from collections import OrderedDict
+from skimage.filters import threshold_mean
+from skimage.measure import regionprops, label
+from skimage.segmentation import clear_border
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import json
+
+
+class SrtRfu16:
+    def __init__(self):
+        self.temp_li = ['Low Temp', 'High Temp']
+        self.grid_cent = None
+        self.row_name = list('ABCD')
+        self.col_name = range(1, 5)
+        self.version = subprocess.check_output(
+            ['git', 'rev-parse', '--short', 'HEAD']).decode('utf-8').strip()
+        self.step_li = ['3', '4', '7']
+        self.dummy_data_path = [
+            pathlib.Path('dummy_data/QuantStep4/q4.json'),
+            pathlib.Path('dummy_data/QuantStep5/q5.json'),
+            pathlib.Path('dummy_data/MeltStep8/melt.json')
+        ]
+        self.dummy_well_name = [
+            [x+str(y) for y in range(1, 5) for x in list('ABCD')],
+            [x+str(y) for y in range(1, 5) for x in list('EFGH')],
+            [x+str(y) for y in range(5, 9) for x in list('ABCD')],
+            [x+str(y) for y in range(5, 9) for x in list('EFGH')],
+            [x+str(y) for y in range(9, 13) for x in list('ABCD')],
+            [x+str(y) for y in range(9, 13) for x in list('EFGH')],
+        ]
+        self.ch_dict = OrderedDict([
+            ('f', 'FAM'),
+            ('h', 'HEX'),
+            ('c', 'Cal Red 610'),
+            ('q6', 'Quasar 670'),
+            ('q7', 'Quasar 705'),
+        ])
+
+    def get_region_li(self, im_labeled, im_gray):
+        region_li = []
+        for region in regionprops(im_labeled, intensity_image=im_gray):
+            region_li.append((region.area, region))
+        sorted_region_li = sorted(
+            region_li, key=lambda tup: tup[0], reverse=True)
+        return [region for area, region in sorted_region_li]
+
+    def get_grid_center(self):
+        self.grid_cent = {}
+        for well, pts in self.grid.items():
+            cent_y = int((pts[3] + pts[1])/2)
+            cent_x = int((pts[2] + pts[0])/2)
+            self.grid_cent[well] = (cent_y, cent_x)
+
+    def create_circular_mask(self, h, w, center, radius):
+        Y, X = np.ogrid[:h, :w]
+        dist_from_center = np.sqrt((X - center[0])**2 + (Y - center[1])**2)
+
+        mask = dist_from_center <= radius
+        return mask
+
+    def calculate_rfu(self, im_path):
+        "Read dummy data"
+        region_sum_dict = {}
+        im_name = im_path.stem
+        _, step, cycle, cam_ind, ch = im_name.split('_')
+        if step in self.step_li:
+            ind = self.step_li.index(step)
+            with open(self.dummy_data_path[ind], 'r') as f:
+                dummy_data = json.load(f)
+            for well in self.dummy_well_name[cam_ind]:
+                if well in dummy_data[self.ch_dict[ch]].keys():
+                    region_sum_dict[well] = dummy_data[self.ch_dict[ch]][well][
+                        int(cycle)]
+                else:
+                    region_sum_dict[well] = np.nan
+        else:
+            raise
+
+        return region_sum_dict
+
+    def open_im(self, im_path):
+        "open images. Designed for adding image rotation for 96well"
+        im = np.array(Image.open(im_path))
+        return im
+
+    def label_image(self, im_path):
+        im_cropped = self.open_im(im_path)
+
+        im_gray = im_cropped.sum(axis=2)
+        cleared = clear_border(im_gray)
+        thresh = threshold_mean(cleared)
+        threshed_im = cleared > thresh
+        return label(threshed_im), im_gray
+
+    def set_grid(self, ref_path, is_outf=False):
+        "get grid by camera from the last cycle"
+        im_path = pathlib.Path(ref_path)
+        self.grid = self.set_grid_single(im_path)
+        self.get_grid_center()
+        if is_outf:
+            grid_json = json.dumps(self.grid_cent)
+            with open("{}/grid.json".format(im_path.parent), "w") as f:
+                json.dump(grid_json, f)
+
+    def set_grid_single(self, im_f):
+        im_labeled, im_gray = self.label_image(im_f)
+        region_li = self.get_region_li(im_labeled, im_gray)
+        well_box = region_li[0].bbox
+        x_li = np.linspace(well_box[1], well_box[3], 5, endpoint=True)
+        y_li = np.linspace(well_box[0], well_box[2], 5, endpoint=True)
+        pts_li = [(x, y) for x in x_li for y in y_li]
+        grid = {}
+        for ind in range(19):
+            i, j = divmod(ind, 5)
+            if j == 4:
+                continue
+            top_left_pt = pts_li[ind]
+            bottom_right_pt = pts_li[ind+6]
+            well = self.get_well_name4grid(i, j)
+            grid[well] = [top_left_pt[1], top_left_pt[0],
+                          bottom_right_pt[1], bottom_right_pt[0]]
+        return grid
+
+    def get_well_name4grid(self, i, j):
+        return self.row_name[j] + str(self.col_name[i])
+
+    def mp_rfu(self, im_path):
+        _path = pathlib.Path(im_path)
+        self.set_grid_json(_path)
+        im = self.open_im(_path)
+        _rfu = self.calculate_rfu(_path)
+        return _rfu
+
+    def set_grid_json(self, im_path):
+        if not isinstance(self.grid_cent, dict):
+            try:
+                with open('{}/grid.json'.format(im_path.parent), 'r') as f:
+                    self.grid_cent = json.load(f)
+            except FileNotFoundError:
+                print('grid json is not found')
+                raise
